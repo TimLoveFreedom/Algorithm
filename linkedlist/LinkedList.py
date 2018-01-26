1f1b42807fdb49c57b2e7e12b32d402418e571b7
TimLoveFreedom <zhangguodong_12@126.com>
2018-01-26 16:40:01 +0800

    linkedlist

--- linkedlist/LinkedList.py
+++ linkedlist/LinkedList.py
@@ -3,6 +3,7 @@ __author__ = 'jellyzhang'
 define the Node
 '''
 class Node(object):
+    __slots__ = ['value', 'next']
     def __init__(self,val,next=None):
         self.value=val
         self.next=next
@@ -14,7 +15,6 @@ class Node(object):
         return  self.next
     def setNext(self,newNext):
         self.next=newNext
-
 
 '''
 define the linkedlist
@@ -34,8 +34,79 @@ class SingleLinkedList(object):
     #append在链表尾部添加元素
     def append(self,item):
         tmp=Node(item)
-        if self._size==0:
-            self._head
+        if self.isEmpty():
+            self._head=tmp
+        else:
+            currentNode=self._head
+            while currentNode.getNext()!=None:
+                currentNode=currentNode.getNext()
+            currentNode.setNext(tmp)
+    #search检索元素是否在链表中
+    def search(self,item):
+        current=self._head
+        isExist=False
+        while current!=None and not isExist:
+            if current.value==item:
+                isExist=True
+            else:
+                current=current.getNext()
+        return  isExist
+    #index索引元素在链表中的位置(存在则返回大于0的正数，不存在返回0)
+    def index(self,item):
+        count=0
+        current = self._head
+        isExist = False
+        while current != None and not isExist:
+            count+=1
+            if current.value == item:
+                isExist = True
+            else:
+                current = current.getNext()
+        if isExist:
+            return count
+        else:
+            return 0
+    #remove删除链表中的某项元素
+    def remove(self,item):
+        current=self._head
+        pre=None
+        while current!=None :
+            if current.getData()==item:
+                if not pre:
+                    self._head=current.getNext()
+                else:
+                    pre.setNext(current.getNext())
+                break
+            else:
+                pre=current
+                current=current.getNext()
+    #insert在链表的指定位置添加节点
+    def insert(self,position,item):
+        if position<0 or position>self._size:
+            raise('position error')
+        tmp=Node(item)
+        if self._size==position:
+            self.append(tmp)
+        else:
+            count=1
+            pre=None
+            current=self._head
+            while count<=position and current!=None:
+                if count==position:
+                    pre=current.getNext()
+                    current.setNext(tmp)
+                    tmp.setNext(pre)
+                else:
+                    current=current.getNext()
+                    count+=1
+            self._size+=1
+
+
+
+
+
+
+
 
 
 
---
+++ linkedlist/LinkedListTest.py
@@ -0,0 +1,48 @@
+__author__ = 'jellyzhang'
+from linkedlist.LinkedList import *
+
+'''
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+'''
+LinkedListA=SingleLinkedList()
+LinkedListA.append(2)
+LinkedListA.append(4)
+LinkedListA.append(6)
+LinkedListB=SingleLinkedList()
+LinkedListB.append(5)
+LinkedListB.append(6)
+LinkedListB.append(4)
+LinkedListB.append(1)
+
+LinkedListResult=SingleLinkedList()
+currentA=LinkedListA._head
+currentB=LinkedListB._head
+tmpTag=0
+while currentA!=None and currentB!=None:
+    resultValue=(currentA.value+currentB.value+tmpTag)%10
+    tmpTag=(currentA.value+currentB.value)//10
+    LinkedListResult.append(resultValue)
+    currentA=currentA.getNext()
+    currentB= currentB.getNext()
+
+
+#多余的节点补上
+if currentA==None and currentB==None:
+    pass
+else:
+    current=currentA if currentB==None else currentB
+    LinkedListResult.append(current.value+tmpTag)
+    while current.getNext()!=None:
+        LinkedListResult.append(current.getNext().value)
+        current=current.getNext()
+
+
+
+######print###################
+head=LinkedListResult._head
+while head!=None:
+    print(head.value)
+    head=head.getNext()
+
